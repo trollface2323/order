3b29232f3a815c8d192ecdbdf1c5814c79295c8d
Sergei <s1adm1npg@yandex.ru>
2020-04-28 22:22:31 +0300

    начал проект

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Weыssssыыlcome to Your Vue.js App"/>
+  <div>
+
+    <div style="background-image: url(/Users/s1sad/Downloads/5bb648692cd8406297588c309fa56def.jpg)">
+      <p>
+        ghjk
+      </p>
+
+    </div>
+
+    <div>
+
+    </div>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
